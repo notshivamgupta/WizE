c897fae89451faca21f2aae2cc6ce1e3bee46cdd
saloni sheth <salonsheth123@gmail.com>
2021-07-01 16:53:31 +0530

    quiz

--- app/src/main/java/com/example/wize/result.java
+++ app/src/main/java/com/example/wize/result.java
@@ -7,21 +7,19 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class result extends AppCompatActivity {
-TextView cor,tim,sec;
+TextView cor,tim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         cor=findViewById(R.id.correct);
         tim=findViewById(R.id.timetaken);
-        sec=findViewById(R.id.Sec);
         int a,time;
         Intent intent=getIntent();
         a=intent.getIntExtra("Correct",0);
         time= intent.getIntExtra("time",0);
         cor.setText(Integer.toString(a));
-        tim.setText(Integer.toString(time/60));
-        sec.setText(Integer.toString(time%60));
+        tim.setText("Time taken-  "+Integer.toString(time/60)+" : "+Integer.toString(time%60));
     }
     @Override
     public void onBackPressed()
